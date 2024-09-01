a06f1339f5cdb752f5ccf6dc54be7904734bc0a3
Tom Morris <tommorris512@icloud.com>
2024-09-01 22:45:37 +0100

    Added longest streak calculation to data processing file

--- utils/data-processor.go
+++ utils/data-processor.go
@@ -47,3 +47,41 @@ func CalculateMostDailyContributions(data types.ContributionCalendar) (int, stri
 
     return maxDailyContributions, maxDailyContributionsDate
 }
+
+/*
+ * Obtains the longest streak of contributions.
+ *
+ * Iterates over each day in the parsed contribution calendar, incrementing the current streak being calculated if a contribution was made, and resetting it otherwise.
+ * A maximum pointer is maintained throughout the iteration to store the longest streak found at any given point.
+ * Once the entire calendar has been iterated over, the longest streak is returned.
+ *
+ * Params:
+ * - data (types.ContributionCalendar): The ContributionCalendar used to obtain the longest contribution streak from.
+ *
+ * Returns:
+ * - (int): The longest contribution streak in the contribution calendar.
+*/
+func CalculateLongestContributionStreak(data types.ContributionCalendar) int {
+    longestStreak := 0
+    streak := 0
+
+    for _, week := range data.Data.User.ContributionsCollection.ContributionCalendar.Weeks {
+        for _, day := range week.ContributionDays {
+            if day.ContributionCount > 0 {
+                streak++
+            } else {
+                if streak > longestStreak {
+                    longestStreak = streak
+                }
+
+                streak = 0
+            }
+        }
+    }
+
+    if streak > longestStreak {
+        longestStreak = streak
+    }
+
+    return longestStreak
+}
