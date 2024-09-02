f27b729d075d9b605e764e82f31c489940681cff
Tom Morris <tommorris512@icloud.com>
2024-09-02 14:57:02 +0100

    Included current streak calculation in contribution handler

--- handlers/contributions.go
+++ handlers/contributions.go
@@ -1,23 +1,25 @@
 package handlers
 
 import (
-    "fmt"
-    "net/http"
-    "github-streak-api/utils"
+	"fmt"
+	"github-streak-api/utils"
+	"net/http"
 )
 
 func ContributionHandler(writer http.ResponseWriter, request *http.Request, username string, token string) {
-    data, err := utils.GetContributionData(username, token)
-    if err != nil {
-        http.Error(writer, "Failed to get contributions", http.StatusInternalServerError)
-        return
-    }
+	data, err := utils.GetContributionData(username, token)
+	if err != nil {
+		http.Error(writer, "Failed to get contributions", http.StatusInternalServerError)
+		return
+	}
 
-    totalContributions := utils.CalculateTotalContributions(data)
-    maxDailyContributions, maxDailyContributionsDate := utils.CalculateMostDailyContributions(data)
-    longestStreak := utils.CalculateLongestContributionStreak(data)
+	totalContributions := utils.CalculateTotalContributions(data)
+	maxDailyContributions, maxDailyContributionsDate := utils.CalculateMostDailyContributions(data)
+	currentStreak := utils.CalculateCurrentContributionStreak(data)
+	longestStreak := utils.CalculateLongestContributionStreak(data)
 
-    fmt.Fprintf(writer, "User %s has made %d contributions this year.\n", username, totalContributions)
-    fmt.Fprintf(writer, "Most contributions in a day: %d on %s\n", maxDailyContributions, maxDailyContributionsDate)
-    fmt.Fprintf(writer, "Longest streak: %d days\n", longestStreak)
+	fmt.Fprintf(writer, "User %s has made %d contributions this year.\n", username, totalContributions)
+	fmt.Fprintf(writer, "Most contributions in a day: %d on %s\n", maxDailyContributions, maxDailyContributionsDate)
+	fmt.Fprintf(writer, "Current streak: %d days\n", currentStreak)
+	fmt.Fprintf(writer, "Longest streak: %d days\n", longestStreak)
 }
